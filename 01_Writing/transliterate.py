19cb17c264dbd4623db9834f1254cb7a3e17ad2e
Lizzy Gabel <eligabel@iu.edu>
2021-05-06 23:56:18 -0400

    AnswersToUnix.txt

--- 01_Writing/transliterate.py
+++ 01_Writing/transliterate.py
@@ -2,7 +2,7 @@
 import sys
 
 #set up dictionary called letter equivalent
-letEquiv = {"A":"A","a":"a","б":"b"}
+letEquiv = {"а":"a", "б":"b", "в":"v", "г":"g", "д":"d", "е":"ye","ё":"yo", "ж":"zh", "з":"z", "и":"i", "й":"y", "к":"k", "л":"l", "м":"m", "н":"n", "о":"o", "п":"p", "р":"r", "с":"s", "т":"t", "у":"u", "ф":"f", "х":"kh", "ц":"ts", "ч":"ch", "ш":"sh", "щ":"sh", "ъ":"", "ы":"y", "ь":"", "э":"e", "ю":"yu", "я":"ya"}
 
 #define main function
 def main():
